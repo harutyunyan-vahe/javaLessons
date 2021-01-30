9b22df540a5f812b694e900768129a523fc42bcf
Vahe Harutyunyan <vahe.harutyunyan.syn@gmail.com>
2021-01-30 14:54:37 +0400

    add lesson 45

---
+++ Lesson45/src/main/java/com/vahe/App.java
@@ -0,0 +1,7 @@
+package com.vahe;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ Lesson45/src/main/java/com/vahe/model/Employee.java
@@ -0,0 +1,5 @@
+package com.vahe.model;
+
+public class Employee {
+    private String name;
+}
